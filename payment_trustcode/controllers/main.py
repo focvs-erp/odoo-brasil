e967574048fd43cd2dabc44a9aa02f2c114a83f6
Marcelo Marcon <marcelo.marcon@ax4b.com>
2022-01-31 10:30:07 -0400

    realizado a inserção da url dentro do iframe, ( ainda em teste )

--- payment_trustcode/controllers/main.py
+++ payment_trustcode/controllers/main.py
@@ -16,15 +16,20 @@ class IuguController(http.Controller):
         request.env['payment.transaction'].sudo().form_feedback(post, 'iugu')
         return "<status>OK</status>"
 
+    # @http.route(
+    #     '/iugu/checkout/redirect', type='http',
+    #     auth='none', methods=['GET', 'POST'])
+    # def iugu_checkout_redirect(self, **post):
+    #     post = post
+    #     request.session['secure_url'] = post.get('secure_url')
+    #     if 'secure_url' in post:
+    #         return redirect('/payment/teste')
+
     @http.route(
         '/iugu/checkout/redirect', type='http',
         auth='none', methods=['GET', 'POST'])
     def iugu_checkout_redirect(self, **post):
         post = post
-        #     return redirect(post['secure_url'])
         if 'secure_url' in post:
-            return redirect('/payment/teste')
+            return redirect(post['secure_url'])
         
-    @http.route('/payment/teste', auth='public', website=True)
-    def index(self, **kw):
-        return http.request.render("payment_trustcode.credit_card_iugu")
