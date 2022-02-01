c4740e132911408a450e0ba5ff0f61ed42a02a87
Marcelo Marcon <marcelo.marcon@ax4b.com>
2022-01-31 21:43:56 -0400

    adicionado localização para o redirect. porem, está com bug

--- payment_trustcode/static/src/js/main.js
+++ payment_trustcode/static/src/js/main.js
@@ -3,5 +3,7 @@ odoo.define("payment_trustcode.Customize", function (require) {
     require("web.dom_ready");
 
     // alert("Hello");
-    alert('Hello')
+    var iframe = document.getElementById('payment-iugu')
+    // console.log(iframe)
+    // parent.location = 'http://localhost:8069/payment/process'
 });
