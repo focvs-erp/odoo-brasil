4c3c8dd3f3f3ca604c4e2f71dc6fafcd8bc945ab
Marcelo Marcon <marcelo.marcon@ax4b.com>
2022-01-31 21:26:18 -0400

    adicionado javascript no modulo payment-trustcode

--- payment_trustcode/static/src/js/main.js
+++ payment_trustcode/static/src/js/main.js
@@ -1,15 +1,7 @@
 "use strict";
-odoo.define(
-    "payment_trustcode.PaymentRedirect",
-    [],
-    function (require) {
-        require("web.dom_ready");
+odoo.define("payment_trustcode.Customize", function (require) {
+    require("web.dom_ready");
 
-        var ajax = require("web.ajax");
-
-        console.log(e);
-        alert("Hello");
-
-        btn.click(_onBtnClick);
-    }
-);
+    // alert("Hello");
+    alert('Hello')
+});
