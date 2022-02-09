4a5b4b94950d404c8e557e96d01517ad7e396a13
Marcelo Marcon <marcelo.marcon@ax4b.com>
2022-02-09 14:37:49 -0400

    adicionado processo de redirect e contagem na pagina de rediricionamento

--- payment_trustcode/static/src/js/main.js
+++ payment_trustcode/static/src/js/main.js
@@ -1,11 +1,30 @@
 odoo.define("payment_trustcode.Customize", function (require) {
-    // require("web.dom_ready");
     "use strict";
-    var session = require("web.session");
 
-    console.log(session);
+    require("web.dom_ready");
 
-    // setTimeout(function () {
-    //     window.location.href = "www.google.com.br";
-    // }, 2000);
+    // Pega o id do span onde está definido a url de
+    // redirecionamento para pagamento.
+    var span = $("#span_id");
+
+    // Verifica se o elemento existe e então
+    // redirecionamento para a tela de pagamento da Iugu
+    if (span.length) {
+
+        var countdown = document.getElementById('countdown')
+        var timeleft = 5;
+
+        var downloadTimer = setInterval(function () {
+            
+            if (timeleft <= 0) {
+                clearInterval(downloadTimer);
+                window.location.href = span.text();
+            } else {
+                countdown.innerHTML = 'Você será redirecionado em ... ' + timeleft;
+            }
+            timeleft -= 1;
+
+        }, 1000);
+    }
+
 });
