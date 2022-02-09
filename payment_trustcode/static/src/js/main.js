e8c947d7146abd0447813f07fdac4790dddfccec
Marcelo Marcon <marcelo.marcon@ax4b.com>
2022-02-09 16:12:29 -0400

    alteração nos ids do elementos para evitar conflito e na mensagem que é mostrada

--- payment_trustcode/static/src/js/main.js
+++ payment_trustcode/static/src/js/main.js
@@ -5,22 +5,23 @@ odoo.define("payment_trustcode.Customize", function (require) {
 
     // Pega o id do span onde está definido a url de
     // redirecionamento para pagamento.
-    var span = $("#span_id");
+    var span = $("#payment_span");
 
     // Verifica se o elemento existe e então
     // redirecionamento para a tela de pagamento da Iugu
     if (span.length) {
 
-        var countdown = document.getElementById('countdown')
-        var timeleft = 5;
+        var countdown = document.getElementById('payment_countdown')
+        var timeleft = 3;
 
         var downloadTimer = setInterval(function () {
             
             if (timeleft <= 0) {
                 clearInterval(downloadTimer);
-                window.location.href = span.text();
+                // window.location.href = span.text();
+                console.log('Redirected')
             } else {
-                countdown.innerHTML = 'Você será redirecionado em ... ' + timeleft;
+                countdown.innerHTML = 'Você será redirecionado em... ' + timeleft;
             }
             timeleft -= 1;
 
