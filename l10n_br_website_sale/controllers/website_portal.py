ccc23ed9ea7552321a35e17eec1a65372c07ae1a
LucasBergaminAX4B <lucas.santos@ax4b.com>
2022-03-08 15:33:23 -0300

    ADD

--- l10n_br_website_sale/controllers/website_portal.py
+++ l10n_br_website_sale/controllers/website_portal.py
@@ -6,21 +6,31 @@ import odoo.addons.website_sale.controllers.main as main
 from odoo.addons.portal.controllers.portal import CustomerPortal
 
 class BrWebsiteMyAccount(CustomerPortal):
-    # def _get_mandatory_billing_fields(self):
-    #     res = super(BrWebsiteMyAccount, self)._get_mandatory_billing_fields()
-    #     return res + [
-    #         "name_responsible",
-    #         "email_responsible",
-    #         "phone_responsible",
-    #         "name_responsible_for_license",
-    #         "email_responsible_for_license",
-    #         "phone_responsible_for_license",
-    #         "is_licence_holder_input",
-    #     ]
+    def remove_post(self, post):
+        fields = ['name_responsible', 'email_responsible', 'phone_responsible', 'checkbox_responsible_license', 'name_responsible_for_license', 'email_responsible_for_license', 'phone_responsible_for_license']
+        for i in fields:
+            del post[i]
+
+    MANDATORY_BILLING_FIELDS = [
+        "name",
+        "phone",
+        "email",
+        "street",
+        "l10n_br_cnpj_cpf",
+        "l10n_br_number",
+        "l10n_br_district",
+        "zipcode",
+        "company_type",
+        "city_id",
+        "state_id",
+        "country_id",
+    ]
+    OPTIONAL_BILLING_FIELDS = ["street2"]
 
     @http.route(["/my/account"], type="http", auth="user", website=True)
     def account(self, redirect=None, **post):
         if "zip" in post:
+            self.remove_post(post)
             post["zipcode"] = post.pop("zip")
         return super(BrWebsiteMyAccount, self).account(
             redirect=redirect, **post
