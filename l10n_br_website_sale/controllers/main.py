54728fb75b5dae7e1080623686f143db0675ed65
Gabriel Gomes Batista <gabriel.batista@ax4b.com>
2022-03-28 19:41:35 -0300

    build new feature

--- l10n_br_website_sale/controllers/main.py
+++ l10n_br_website_sale/controllers/main.py
@@ -10,6 +10,7 @@ from odoo.addons.portal.controllers.portal import CustomerPortal
 
 
 class L10nBrWebsiteSale(main.WebsiteSale):
+
     def _get_mandatory_billing_fields(self):
         res = super(L10nBrWebsiteSale, self)._get_mandatory_billing_fields()
         res.remove("city")
@@ -142,8 +143,128 @@ class L10nBrWebsiteSale(main.WebsiteSale):
         new_values["l10n_br_number"] = values.get("l10n_br_number", None)
         new_values["street2"] = values.get("street2", None)
         new_values["l10n_br_district"] = values.get("l10n_br_district", None)
+        # new_values['child_ids'] = self.get_child_ids(values)
 
         return new_values, errors, error_msg
+
+    # @http.route(['/shop/address'], type='http', methods=['GET', 'POST'], auth="public", website=True, sitemap=False)
+    # def address(self, **kw):
+    #     Partner = request.env['res.partner'].with_context(show_address=1).sudo()
+    #     order = request.website.sale_get_order()
+
+    #     redirection = self.checkout_redirection(order)
+    #     if redirection:
+    #         return redirection
+
+    #     mode = (False, False)
+    #     can_edit_vat = False
+    #     values, errors = {}, {}
+
+    #     partner_id = int(kw.get('partner_id', -1))
+
+    #     # IF PUBLIC ORDER
+    #     if order.partner_id.id == request.website.user_id.sudo().partner_id.id:
+    #         mode = ('new', 'billing')
+    #         can_edit_vat = True
+    #     # IF ORDER LINKED TO A PARTNER
+    #     else:
+    #         if partner_id > 0:
+    #             if partner_id == order.partner_id.id:
+    #                 mode = ('edit', 'billing')
+    #                 can_edit_vat = order.partner_id.can_edit_vat()
+    #             else:
+    #                 shippings = Partner.search([('id', 'child_of', order.partner_id.commercial_partner_id.ids)])
+    #                 if order.partner_id.commercial_partner_id.id == partner_id:
+    #                     mode = ('new', 'shipping')
+    #                     partner_id = -1
+    #                 elif partner_id in shippings.mapped('id'):
+    #                     mode = ('edit', 'shipping')
+    #                 else:
+    #                     return Forbidden()
+    #             if mode and partner_id != -1:
+    #                 values = Partner.browse(partner_id)
+    #         elif partner_id == -1:
+    #             mode = ('new', 'shipping')
+    #         else: # no mode - refresh without post?
+    #             return request.redirect('/shop/checkout')
+
+    #     # IF POSTED
+    #     if 'submitted' in kw:
+    #         pre_values = self.values_preprocess(order, mode, kw)
+    #         errors, error_msg = self.checkout_form_validate(mode, kw, pre_values)
+    #         post, errors, error_msg = self.values_postprocess(order, mode, pre_values, errors, error_msg)
+
+    #         if errors:
+    #             errors['error_message'] = error_msg
+    #             values = kw
+    #             values['child_ids'] = self.get_child_ids(kw)
+    #         else:
+    #             partner_id = self._checkout_form_save(mode, post, kw)
+    #             if mode[1] == 'billing':
+    #                 order.partner_id = partner_id
+    #                 order.with_context(not_self_saleperson=True).onchange_partner_id()
+    #                 # This is the *only* thing that the front end user will see/edit anyway when choosing billing address
+    #                 order.partner_invoice_id = partner_id
+    #                 if not kw.get('use_same'):
+    #                     kw['callback'] = kw.get('callback') or \
+    #                         (not order.only_services and (mode[0] == 'edit' and '/shop/checkout' or '/shop/address'))
+    #             elif mode[1] == 'shipping':
+    #                 order.partner_shipping_id = partner_id
+
+    #             # TDE FIXME: don't ever do this
+    #             order.message_partner_ids = [(4, partner_id), (3, request.website.partner_id.id)]
+    #             if not errors:
+    #                 return request.redirect(kw.get('callback') or '/shop/confirm_order')
+
+    #     render_values = {
+    #         'website_sale_order': order,
+    #         'partner_id': partner_id,
+    #         'mode': mode,
+    #         'checkout': values,
+    #         'can_edit_vat': can_edit_vat,
+    #         'error': errors,
+    #         'callback': kw.get('callback'),
+    #         'only_services': order and order.only_services,
+    #     }
+    #     render_values.update(self._get_country_related_render_values(kw, render_values))
+    #     return request.render("website_sale.address", render_values)
+
+    def get_child_ids(self, vals):
+        if 'child_ids' in vals:
+            return self.build_child_ids_partner(vals)
+        else:
+            return self.build_child_ids_kw(vals)
+
+    
+    def build_child_ids_partner(self, values):
+        child_ids = []
+        for child in filter(lambda ch : ch['website_contact'], values['child_ids']):
+            child_ids.append({
+                'name': child.name,
+                'phone':child.phone,
+                'email':child.email,
+                'website_contact': True
+            })
+
+        return child_ids
+
+    def build_child_ids_kw(self, values):
+        child_ids = []
+        child_ids.append({
+            'name': values.get('name_responsible', ''),
+            'phone':values.get('phone_responsible', ''),
+            'email':values.get('email_responsible', ''),
+            'website_contact': True
+        })
+        child_ids.append({
+            'name': values.get('name_responsible_for_license', ''),
+            'phone':values.get('phone_responsible_for_license', ''),
+            'email':values.get('email_responsible_for_license',''),
+            'website_contact': True
+        })
+
+        return child_ids
+
 
     # AX4B - LICENSE HOLDER
     def _create_partner_contact(self, partner_id, all_values):
@@ -212,6 +333,8 @@ class L10nBrWebsiteSale(main.WebsiteSale):
                     and partner_id != order.partner_id.id
                 ):
                     return Forbidden()
+                
+                checkout.pop('child_ids')
 
                 Partner.browse(partner_id).sudo().write(checkout)
 
@@ -231,7 +354,16 @@ class L10nBrWebsiteSale(main.WebsiteSale):
             result.qcontext["state"] = partner_id.state_id
         if "city_id" in kw and kw["city_id"]:
             result.qcontext["city"] = request.env['res.city'].browse(kw["city_id"])
+        
+        # if 'checkout' in result.qcontext and 'child_ids' not in result.qcontext['checkout']:
+            # result.qcontext["checkout"]["child_ids"] = self.get_child_ids(kw)
+        result.qcontext["child_ids"] = self.get_child_ids(partner_id if 'child_ids' in result.qcontext['checkout'] else kw)
+
         return result
+        
+
+           
+
 
     @http.route(
         ["/shop/zip_search"],
